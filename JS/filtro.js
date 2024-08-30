35b54ce70f172befd226a364426feaf3d867328a
ClemenPalazzo <palazzocle@gmail.com>
2024-08-30 20:58:04 +0800

    style y js cambiosd

--- JS/filtro.js
+++ JS/filtro.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const products = [
     { name: "CyberPunk", img: "./img/DayGone.jpeg", description: "Juego CyberPunk" },
@@ -42,34 +41,4 @@ function displaySearchResults(products) {
     
     const searchModal = new bootstrap.Modal(modalContainer);
     searchModal.show();
-=======
-// filtro.js
-
-function filterProducts() {
-    const input = document.getElementById('navbar-search-input').value.toLowerCase();
-    const cards = document.querySelectorAll('.card-destacados, .promotion-card');
-
-    const searchResults = document.getElementById('searchResults');
-    searchResults.innerHTML = ''; // Limpiar resultados previos
-
-    cards.forEach((card) => {
-        const title = card.querySelector('.card-title').textContent.toLowerCase();
-        const cardContainer = card.closest('.col');
-
-        if (title.includes(input)) {
-            cardContainer.style.display = 'block';
-            // Clonar la tarjeta y agregarla a los resultados de la búsqueda
-            const clone = card.cloneNode(true);
-            searchResults.appendChild(clone);
-        } else {
-            cardContainer.style.display = 'none';
-        }
-    });
-
-    // Mostrar el modal si hay resultados
-    if (searchResults.children.length > 0) {
-        const modal = new bootstrap.Modal(document.getElementById('searchModal'));
-        modal.show();
-    }
->>>>>>> 620ad05cae5dafdbe0db19e48b0bbbf1bc960e51
 }
