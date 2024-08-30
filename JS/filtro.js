1fdc24dd00d4c589f982c656a73543da654d099b
ClemenPalazzo <palazzocle@gmail.com>
2024-08-30 20:27:17 +0800

    toque filtros

--- JS/filtro.js
+++ JS/filtro.js
@@ -1,24 +1,22 @@
-// Datos de ejemplo del catálogo de productos
+
 const products = [
     { name: "CyberPunk", img: "./img/DayGone.jpeg", description: "Juego CyberPunk" },
-
-    { name: "Beyond Two Souls", img: "./img/Beyond Two Souls Poster.jpeg", description: "Juego Beyond Two Souls" },
-
- 
-
-    { name: "Memoria RAM 16GB", img: "./img/BioInfinity.jpeg", description: "Memoria RAM 16GB" },
-    { name: "Fuente de Poder 500W", img: "./img/detroit.jpeg", description: "Fuente de Poder 500W" },
-    // Añade más productos aquí
+    { name: "Beyond two souls", img: "./img/Beyond Two Souls Poster.jpeg", description: "Juego Beyond Two Souls" },
+    { name: "Infinity", img: "/img/Stray.jpeg", description: "Stray" },
+    { name: "Death stranding", img: "./img/Death Stranding art.jpeg", description: "Death stranding" },
+    { name: "Infinity", img: "./img/BioInfinity.jpeg", description: "Bioshok infinity" },
+    { name: "Detroit", img: "./img/detroit.jpeg", description: "Detroit become humane" },
+   
 ];
 
-// Función para manejar la búsqueda
+
 function handleSearch() {
     const query = document.getElementById("navSearchInput").value.toLowerCase();
     const filteredProducts = products.filter(product => product.name.toLowerCase().includes(query));
     displaySearchResults(filteredProducts);
 }
 
-// Función para mostrar los resultados de la búsqueda en un modal
+
 function displaySearchResults(products) {
     let modalContent = `<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Resultados de la búsqueda</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">`;
 
@@ -32,7 +30,7 @@ function displaySearchResults(products) {
 
     modalContent += `</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button></div></div></div>`;
     
-    // Crear el modal
+
     const modalContainer = document.createElement("div");
     modalContainer.className = "modal fade";
     modalContainer.id = "searchModal";
