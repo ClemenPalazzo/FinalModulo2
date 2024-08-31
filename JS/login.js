ef7cca5a696ca800f0f7b1bbf7f7139e800bd8bf
Santiago Rodriguez <santiara010@gmail.com>
2024-08-31 13:06:52 -0300

    Agregué la lógica para ingresar con el login cargando los datos desde el local

--- JS/login.js
+++ JS/login.js
@@ -7,13 +7,20 @@ console.log(Administrador)
 const formularioLogin = document.getElementById('formLogin');
 const emailL = document.getElementById('emailL');
 const passwordL = document.getElementById('passwordL');
+let n=false;
 
 //funciones login
 const analizarCuenta = (e) =>{
     e.preventDefault();
     if (emailL.value === Administrador.mail && passwordL.value === Administrador.contraseña){
-console.log("dentro del if")
-window.location.href = '../index.html'
+        localStorage.setItem('Adminkey',JSON.stringify(Administrador));
+        window.location.href = '../index.html'
+    } else if (n == false){
+        const error = document.createElement('h6');
+        error.innerHTML = 'El email o la contraseña es incorrecta'
+        error.className = 'text-center text-danger'
+        formularioLogin.prepend (error)
+        n = true;
     }
 }
 //logica del crud (Login)
