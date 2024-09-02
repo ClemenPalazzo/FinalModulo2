749bd6af88c59c7667d45c67fc805e618993b949
Santiago Rodriguez <santiara010@gmail.com>
2024-09-02 00:08:22 -0300

    Los usuarios registrador y el admin ya pueden ingresar

--- JS/login.js
+++ JS/login.js
@@ -1,20 +1,27 @@
 import { Contacto } from "./classContactos.js";
 
 //Variables 
-const Administrador = new Contacto ('Santiago', 'Rodriguez', 'Santirod7','santiara010@gmail.com','3813566953','santi123','hola')
+const Administrador = new Contacto ('Santiago', 'Rodriguez', 'Santirod7','santiara010@gmail.com','3813566953','santi123')
 console.log(Administrador)
 
 const formularioLogin = document.getElementById('formLogin');
 const emailL = document.getElementById('emailL');
 const passwordL = document.getElementById('passwordL');
-let n=false;
+let n = false;
+const listaUsuario = JSON.parse(localStorage.getItem('listaUsuariokey')) || [];
 
 //funciones login
 const analizarCuenta = (e) =>{
     e.preventDefault();
-    if (emailL.value === Administrador.mail && passwordL.value === Administrador.contraseña){
+    if (emailL.value === Administrador.mail && passwordL.value === Administrador.password){
         localStorage.setItem('Adminkey',JSON.stringify(Administrador));
         window.location.href = '../index.html'
+    } else if (listaUsuario){
+        const mailRegistrado = listaUsuario.find(listaUsuario => listaUsuario.mail === emailL.value)
+        const passRegistrado = listaUsuario.find(listaUsuario => listaUsuario.password === passwordL.value)
+        if(mailRegistrado && passRegistrado){
+            window.location.href = '../index.html'
+        }
     } else if (n == false){
         const error = document.createElement('h6');
         error.innerHTML = 'El email o la contraseña es incorrecta'
