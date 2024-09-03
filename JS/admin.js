ede3cd62c32a9b58b06be3ccf85d23cfb8f54f7b
ValentinaPalazzo <tymypvp@gmail.com>
2024-09-02 21:25:48 -0300

    pull y push

--- JS/admin.js
+++ JS/admin.js
@@ -1,4 +1,3 @@
-
 class Juego {
     constructor(id, nombreJuego, descripcionJuego, precioJuego, categoriaJuego, desarrolladorJuego, requisitosJuego, tipoJuego, imagenJuego, reseñaJuego) {
         this.id = id || this.generarId();
@@ -32,7 +31,6 @@ class Juego {
         };
     }
 }
-
 
 function guardarJuegos(juegos) {
     localStorage.setItem('juegos', JSON.stringify(juegos.map(juego => juego.toJSON())));
@@ -85,7 +83,6 @@ function renderizarTabla() {
         tbody.appendChild(fila);
     });
 
-
     document.querySelectorAll('.ver-detalle').forEach(btn => {
         btn.addEventListener('click', (event) => {
             const id = event.target.getAttribute('data-id');
@@ -107,7 +104,6 @@ function renderizarTabla() {
         });
     });
 }
-
 
 function abrirModal(juego = null) {
     const modalLabel = document.getElementById('juegoModalLabel');
@@ -173,7 +169,6 @@ function abrirModal(juego = null) {
     };
 }
 
-
 function editarJuego(id) {
     const juego = juegos.find(j => j.id === id);
     abrirModal(juego);
@@ -194,13 +189,12 @@ function verDetalleJuego(id) {
     /*
     const juego = juegos.find(j => j.id === id);
     if (juego) {
-        alert(`Detalles del juego:\n\nNombre: ${juego.nombreJuego}\nDescripción: ${juego.descripcionJuego}\nPrecio: ${juego.precioJuego}\n...`);
+        alert(Detalles del juego:\n\nNombre: ${juego.nombreJuego}\nDescripción: ${juego.descripcionJuego}\nPrecio: ${juego.precioJuego}\n...);
     } else {
         console.log('Juego no encontrado');
     }
     */
 }
-
 
 document.getElementById('btnNuevo').addEventListener('click', () => {
     abrirModal();
